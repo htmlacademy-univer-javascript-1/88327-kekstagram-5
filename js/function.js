98440a761280ab07716fa61ef95d4678dbe85e5a
Nikita <greattuz@gmail.com>
2024-11-29 09:56:38 +0700

    oncу more

--- js/function.js
+++ js/function.js
@@ -29,11 +29,11 @@ console.log(checkStringLength('Абдулнасир', 13));
 console.log(isPalindrom('-saippuak  ivikaUpp ias-'));
 console.log(isPalindrom('asdassasdasxc'));
 
-console.log(getNumbersFromString('2023 год'));            // 2023
-console.log(getNumbersFromString('ECMAScript 2022'));     // 2022
+console.log(getNumbersFromString('2023 год')); // 2023
+console.log(getNumbersFromString('ECMAScript 2022')); // 2022
 console.log(getNumbersFromString('1 кефир, 0.5 батона')); // 105
-console.log(getNumbersFromString('агент 007'));           // 7
-console.log(getNumbersFromString('а я томат'));           // NaN
+console.log(getNumbersFromString('агент 007')); // 7
+console.log(getNumbersFromString('а я томат')); // NaN
 console.log(getNumbersFromString(2023)); // 2023
-console.log(getNumbersFromString(-1));   // 1
-console.log(getNumbersFromString(1.5));  // 15
+console.log(getNumbersFromString(-1)); // 1
+console.log(getNumbersFromString(1.5)); // 15
