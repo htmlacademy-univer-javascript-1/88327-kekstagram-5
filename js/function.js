dd77225c01e0d81de006f1cac8043bcb615da68d
Nikita <greattuz@gmail.com>
2024-11-29 08:45:48 +0700

    подправил код под проверки

--- js/function.js
+++ js/function.js
@@ -1,39 +1,39 @@
-function checkStringLength( str, count ){
+function checkStringLength(str, count){
   return str.length <= count;
 }
 
-function isPalindrom( str ){
+function isPalindrom(str){
 
-  str = str.replaceAll( /\s/g, '' ).toLowerCase()
-  var checkStr = ""
+  str = str.replaceAll(/\s/g, '').toLowerCase();
+  let checkStr = "";
 
-  for ( let i = str.length-1; i>=0 ; i--){
-    checkStr += str[i]
+  for (let i = str.length-1; i>=0 ; i--){
+    checkStr += str[i];
   }
 
-  return checkStr == str
+  return checkStr == str;
 }
 
-function getNumbersFromString( str ){
+function getNumbersFromString(str){
 
-  str = str.toString().replaceAll( /\D/g, '' ).toLowerCase()
-  str = parseInt(str)
-  return str
+  str = str.toString().replaceAll(/\D/g, '').toLowerCase();
+  str = parseInt(str);
+  return str;
 
 }
 
-// console.log( checkStringLength( 'Абдулнасир', 9 ) )
-// console.log( checkStringLength( 'Абдулнасир', 10 ) )
-// console.log( checkStringLength( 'Абдулнасир', 13 ) )
+console.log(checkStringLength('Абдулнасир', 9));
+console.log(checkStringLength( 'Абдулнасир', 10));
+console.log(checkStringLength( 'Абдулнасир', 13));
 
-// console.log( isPalindrom( '-saippuak  ivikaUpp ias-' ) )
-// console.log( isPalindrom( 'asdassasdasxc' ) )
+console.log(isPalindrom( '-saippuak  ivikaUpp ias-'));
+console.log(isPalindrom( 'asdassasdasxc'));
 
-// getNumbersFromString('2023 год');            // 2023
-// getNumbersFromString('ECMAScript 2022');     // 2022
-// getNumbersFromString('1 кефир, 0.5 батона'); // 105
-// getNumbersFromString('агент 007');           // 7
-// getNumbersFromString('а я томат');           // NaN
-// getNumbersFromString(2023); // 2023
-// getNumbersFromString(-1);   // 1
-// getNumbersFromString(1.5);  // 15
+console.log(getNumbersFromString('2023 год'));            // 2023
+console.log(getNumbersFromString('ECMAScript 2022'));     // 2022
+console.log(getNumbersFromString('1 кефир, 0.5 батона')); // 105
+console.log(getNumbersFromString('агент 007'));           // 7
+console.log(getNumbersFromString('а я томат'));           // NaN
+console.log(getNumbersFromString(2023)); // 2023
+console.log(getNumbersFromString(-1));   // 1
+console.log(getNumbersFromString(1.5));  // 15
