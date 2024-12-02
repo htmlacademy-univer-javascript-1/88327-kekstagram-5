00b518451c0d5591f5ba10ca6d856f683a7a0121
Nikita <greattuz@gmail.com>
2024-12-02 15:13:57 +0700

    правки

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 function generateData(){
 
-  let res = [];
+  const res = [];
   for(let i = 1; i <= 25; i++){
-    let item = {};
+    const item = {};
     item.id = i;
-    item.url = 'photos/' + i + '.jpg';
-    item.description = "Фотография #" + i;
+    item.url = `photos/${i}.jpg`;
+    item.description = `Фотография #${i}`;
     item.likes = getRndInteger(15, 200);
-    item.comments = []
-    let commentsCount = getRndInteger(0, 15);
+    item.comments = [];
+    const commentsCount = getRndInteger(0, 15);
     for(let j = 0; j <= commentsCount; j++){
-      let comment = {};
+      const comment = {};
       comment.id = i*100 + j;
-      comment.avatar = "img/avatar-" + getRndInteger(1, 6) + ".svg";
+      comment.avatar = `img/avatar-${getRndInteger(1, 6)}.svg`;
       comment.name = getName();
       comment.message = getMessage();
       item.comments.push(comment);
     }
-    res.push(item)
+    res.push(item);
   }
 
   return res;
@@ -25,16 +25,16 @@ function generateData(){
 }
 
 function getMessage(){
-  let res = [];
-  let messages = [
-                  'Всё отлично!',
-                  'В целом всё неплохо. Но не всё.',
-                  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-                  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-                  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-                  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-                ];
-  let messageCount = getRndInteger(0, 1);
+  const res = [];
+  const messages = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
+  const messageCount = getRndInteger(0, 1);
   for(let i = 0; i <= messageCount; i++){
     res.push(messages[getRndInteger(0, messages.length)]);
   }
@@ -42,7 +42,7 @@ function getMessage(){
 }
 
 function getName(){
-  let names = [
+  const names = [
     'Петр',
     'Анна',
     'Дмитрий',
@@ -58,24 +58,10 @@ function getName(){
 
 }
 
-
-function newComment( ){
-  let x;
-  if (typeof x !== undefined){
-    x = 22
-  } else {
-    x = 2
-  }
-
-  console.log(x);
-  // let comment.id = 2;
-
-}
-
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 
-x = generateData()
-console.log(x)
+let data = generateData()
+// console.log( data )
