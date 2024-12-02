1cb05cf67319f9f8b3f938288fb0ef71e0c48b6a
Nikita <greattuz@gmail.com>
2024-12-02 15:16:51 +0700

    правки

--- js/main.js
+++ js/main.js
@@ -62,6 +62,6 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-const data = generateData();
+generateData()
+// const data = generateData();
 // console.log( data )
