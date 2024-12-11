9244b4dbe37f9c9b43a6b794b0f344ef18e3d67b
Nikita <greattuz@gmail.com>
2024-12-11 10:33:30 +0700

    правки

--- js/view.js
+++ js/view.js
@@ -1,16 +1,16 @@
-let pictureTemplate = document.querySelector('#picture').content.querySelector(".picture");
-let picturesList = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesList = document.querySelector('.pictures');
 
-export function picturesSetup( data ){
+export function picturesSetup(data){
 
   Array.from(data, function(item){
-    let picture = pictureTemplate.cloneNode(true);
-    let pictureImg = picture.querySelector('.picture__img');
+    const picture = pictureTemplate.cloneNode(true);
+    const pictureImg = picture.querySelector('.picture__img');
     pictureImg.setAttribute('src', item.url);
     pictureImg.setAttribute('alt', item.description);
     picture.querySelector('.picture__likes').textContent = item.likes;
     picture.querySelector('.picture__comments').textContent = item.comments.length;
     picturesList.appendChild(picture);
-  })
+  });
 
 }
