7d6ea7a301595e81cea995988c4927879c854549
Nikita <greattuz@gmail.com>
2024-12-11 10:41:49 +0700

    правки

--- js/view.js
+++ js/view.js
@@ -2,15 +2,15 @@ const pictureTemplate = document.querySelector('#picture').content.querySelector
 const picturesList = document.querySelector('.pictures');
 
 export function picturesSetup(data){
+  Array.from(data, pictureAdd);
+}
 
-  Array.from(data, function(item){
-    const picture = pictureTemplate.cloneNode(true);
-    const pictureImg = picture.querySelector('.picture__img');
-    pictureImg.setAttribute('src', item.url);
-    pictureImg.setAttribute('alt', item.description);
-    picture.querySelector('.picture__likes').textContent = item.likes;
-    picture.querySelector('.picture__comments').textContent = item.comments.length;
-    picturesList.appendChild(picture);
-  });
-
+function pictureAdd(item){
+  const picture = pictureTemplate.cloneNode(true);
+  const pictureImg = picture.querySelector('.picture__img');
+  pictureImg.setAttribute('src', item.url);
+  pictureImg.setAttribute('alt', item.description);
+  picture.querySelector('.picture__likes').textContent = item.likes;
+  picture.querySelector('.picture__comments').textContent = item.comments.length;
+  picturesList.appendChild(picture);
 }
